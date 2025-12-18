df6d79911bd08b569b4bd99e6c2b065219a87176
Yash Verma <helloyashverma@gmail.com>
2025-12-18 19:56:09 +0530

    add new chnages

--- app/link/[username]/Client.tsx
+++ app/link/[username]/Client.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Client = ({ creator, blocks }: { creator: any; blocks: any[] }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-[50%] mx-auto">
+    <div className="flex flex-col items-center justify-center h-screen max-w-[400px] mx-auto">
       {blocks.map((block) => {
         return (
           <LiquidRenderer

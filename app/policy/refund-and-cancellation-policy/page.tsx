01d40409bd3f26b9fccf39a046b29710d49fb72d
chhavey <guptachhavi880@gmail.com>
2025-12-23 21:51:17 +0530

    policy page footer fix

--- app/policy/refund-and-cancellation-policy/page.tsx
+++ app/policy/refund-and-cancellation-policy/page.tsx
@@ -259,7 +259,7 @@ export default function RefundPage() {
                 </h3>
               </div>
               <p className="text-sm font-semibold text-black/80 max-w-2xl mx-auto">
-                Potatopay speaks your language, fits your flow, and supports you from Day 1. No minimum followers. No long waitlists.                <a
+                Potatopay speaks your language, fits your flow, and supports you from Day 1. No minimum followers. No long waitlists.  Get in touch -           <a
                   href="mailto:support@potatopay.co"
                   className="underline font-black"
                 >
@@ -269,8 +269,53 @@ export default function RefundPage() {
             </div>
 
             {/* Footer links and copyright */}
-            <div className="border-t-[2px] border-black/20 pt-8 flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
+            <div className="border-t-[2px] border-black/20 pt-8 flex flex-col gap-6 items-center md:flex-row md:items-center md:justify-between">
               <div className="space-y-1">
+                <div className="sm:hidden flex items-center justify-center gap-2">
+                  <a target="_blank" href="https://x.com/potatopayco">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-twitter-x" viewBox="0 0 16 16" id="Twitter-X--Streamline-Bootstrap" height="20" width="20">
+                      <desc>
+                        Twitter X Streamline Icon: https://streamlinehq.com
+                      </desc>
+                      <path d="M12.6 0.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867 -5.07 -4.425 5.07H0.316l5.733 -6.57L0 0.75h5.063l3.495 4.633L12.601 0.75Zm-0.86 13.028h1.36L4.323 2.145H2.865z" strokeWidth="2"></path>
+                    </svg>
+                  </a>
+
+                  <a target="_blank" href="https://www.instagram.com/potatopay.co">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" id="Instagram--Streamline-Core" height="23" width="23" className="-mb-1">
+                      <desc>
+                        Instagram Streamline Icon: https://streamlinehq.com
+                      </desc>
+                      <g id="instagram">
+                        <g id="Group 4546">
+                          <path id="Vector" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M10.3332 3.64404c-0.1381 0 -0.25 -0.11193 -0.25 -0.25s0.1119 -0.25 0.25 -0.25" strokeWidth="2"></path>
+                          <path id="Vector_2" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M10.3332 3.64404c0.1381 0 0.25 -0.11193 0.25 -0.25s-0.1119 -0.25 -0.25 -0.25" strokeWidth="2"></path>
+                        </g>
+                        <path id="Rectangle 2" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M0.858276 3.43141c0 -1.42103 1.151974 -2.573012 2.573014 -2.573012h6.86141c1.421 0 2.573 1.151982 2.573 2.573012v6.86139c0 1.421 -1.152 2.573 -2.573 2.573H3.43129c-1.42104 0 -2.573014 -1.152 -2.573014 -2.573V3.43141Z" strokeWidth="2"></path>
+                        <path id="Ellipse 11" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M4.312 6.862a2.55 2.55 0 1 0 5.1 0 2.55 2.55 0 1 0 -5.1 0" strokeWidth="2"></path>
+                      </g>
+                    </svg>
+                  </a>
+
+                  <a target="_blank" href="https://youtube.com/@potatopayco?si=Nrd__8chUv-pUuWA">
+                    <svg
+                      width="25"
+                      height="25"
+                      fill="currentColor"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="-my-1"
+                    >
+                      <path d="M508.64,148.79c0-45-33.1-81.2-74-81.2C379.24,65,322.74,64,265,64H247c-57.6,0-114.2,1-169.6,3.6-40.8,0-73.9,36.4-73.9,81.4C1,184.59-.06,220.19,0,255.79q-.15,53.4,3.4,106.9c0,45,33.1,81.5,73.9,81.5,58.2,2.7,117.9,3.9,178.6,3.8q91.2.3,178.6-3.8c40.9,0,74-36.5,74-81.5,2.4-35.7,3.5-71.3,3.4-107Q512.24,202.29,508.64,148.79ZM207,353.89V157.39l145,98.2Z" />
+                    </svg>
+                  </a>
+
+                  <a target="_blank" href="https://www.linkedin.com/company/potatopay/">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50">
+                      <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
+                    </svg>
+                  </a>
+                </div>
                 <p className="text-sm font-black">
                   {/* © {new Date().getFullYear()} Potatopay.co */}
                   © 2025 Potatopay Private Limited.
@@ -279,8 +324,8 @@ export default function RefundPage() {
                   India&apos;s #1st Tipping Platform For Live Streamers.
                 </p>
               </div>
-              <div className="flex flex-col items-end justify-center gap-3">
-                <div className="flex items-center justify-start gap-2">
+              <div className="flex flex-col items-center md:items-end justify-center gap-3">
+                <div className="hidden sm:flex items-center justify-start gap-2">
                   <a target="_blank" href="https://x.com/potatopayco">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-twitter-x" viewBox="0 0 16 16" id="Twitter-X--Streamline-Bootstrap" height="20" width="20">
                       <desc>

335cd5330f9528e1c27a7238851c692984e2ffc2
chhavey <guptachhavi880@gmail.com>
2025-12-23 22:14:45 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,77 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Potatopay.co - The Future of Digital Payments & Fun Fan Funding",
-  description: "The Future of Digital Payments & Fun Fan Funding",
+  description: "Turn boring UPI payments into exciting, gamified tips. More earnings for creators, more fun for fans.",
+  keywords: [
+    "superchat",
+    "tip",
+    "tips",
+    "tipping",
+    "earn",
+    "earning",
+    "stream",
+    "streaming",
+    "streamer",
+    "live",
+    "live streaming",
+    "gamer",
+    "game",
+    "gaming",
+    "irl",
+    "irl streaming",
+    "youtube gaming",
+    "twitch streaming",
+    "facebook gaming",
+    "sponsor",
+    "potatopay",
+    "potatopay.co",
+    "rachitroo"
+  ],
+  authors: [{ name: "Potatopay" }],
+  creator: "Potatopay",
+  publisher: "Potatopay",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://potatopay.co"),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_IN",
+    url: "/",
+    siteName: "Potatopay.co",
+    title: "Potatopay.co - The Future of Digital Payments & Fun Fan Funding",
+    description: "Turn boring UPI payments into exciting, gamified tips. More earnings for creators, more fun for fans.",
+    images: [
+      {
+        url: "/icon.svg",
+        width: 1200,
+        height: 630,
+        alt: "Potatopay - India's #1st Tipping Platform For Live Streamers",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Potatopay.co - The Future of Digital Payments & Fun Fan Funding",
+    description: "Turn boring UPI payments into exciting, gamified tips. More earnings for creators, more fun for fans.",
+    images: ["/icon.svg"],
+    creator: "@potatopayco",
+    site: "@potatopayco",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   icons: {
     icon: "/icon.svg",
+    apple: "/icon.svg",
   },
 };
 
